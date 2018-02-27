f00c14cd4ffa3edc46ef9e6e57e0b7e98c0efcb1
HopeWu <767088240@qq.com>
2018-02-27 12:01:28 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+MONGO_URL = 'localhost'
+MONGO_DB = 'taobao'
+
+SERVICE_ARGS=['--load-images=false', '--disk-cache=true']
+
+# the following KEY_WORD serves as both the name of the table in mongo and the key word of our search
+KEY_WORD='kouqing'
---
+++ spider.py
@@ -0,0 +1,74 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+from pyquery import PyQuery as pc
+from config import *
+import pymongo
+
+browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
+wait = WebDriverWait(browser, 20)
+browser.set_window_size(1400, 900)
+
+browser2 = webdriver.PhantomJS(service_args=SERVICE_ARGS)
+wait2 = WebDriverWait(browser2, 20)
+browser2.set_window_size(1400, 900)
+
+MAX_PAGE = 9
+
+def savePicSet(set_addr):
+    next_pic_addr = set_addr + '#p=2'
+    browser2.get(next_pic_addr)
+    total = wait2.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#imgTotal'))
+    ).text
+    print(next_pic_addr)
+    print(total)
+    for page in range(3, total +1):
+        tail = '#p=' + str(page)
+        next_pic_addr = set_addr + tail
+        browser2.get(next_pic_addr)
+
+def getPicSet():
+    print(" in get pic set")
+    next_page = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '#pagebar > input.enter'))
+    )
+    next_page = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'body > div.See_layer > div.See_main'))
+    )
+    html = browser.page_source
+    doc = pc(html)
+    items = doc('#cover-list > li').items()
+    for item in items:
+        set_addr = item.find('div > a').attr('href')
+        print(item.find('div > a').attr('href'))
+        savePicSet(set_addr)
+        print('\n')
+
+def nextPage(page_num):
+    print('next page processing', page_num)
+    _input = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#pagebar > input.pg_text'))
+    )
+    submit = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '#pagebar > input.enter'))
+    )
+    _input.clear()
+    _input.send_keys(page_num)
+    submit.click()
+    wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#pagebar > span.current'), str(page_num)))
+    getPicSet()
+
+
+
+def main():
+    browser.get("http://photo.sina.com.cn/wit/")
+    getPicSet()
+    for page_num in range(2, MAX_PAGE + 1):
+        nextPage(page_num)
+
+if __name__ =='__main__':
+    main()
